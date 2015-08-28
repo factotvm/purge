ddaf4527572a2954a5f2553cd12b90542675c950
Daniel Thompson <daniel@ionize.net>
2015-08-28 10:11:43 -0700

    Remove trailing spaces

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
-{ 
+{
     if (argc < 2) {
       printf("Usage: %s directory\n", argv[0]);
       exit(EXIT_FAILURE);
-    } 
+    }
 
     printf("**** PURGE ****\n");
     purge(argv[1]);
